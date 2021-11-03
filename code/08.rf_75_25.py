2ebed5ad92f91f3a6320bf9816396b1b8e39abfe
twyunting <yc6705a@american.edu>
2021-11-02 22:55:37 -0400

    recode the order

--- code/08.rf_75_25.py
+++ code/08.rf_75_25.py
@@ -81,7 +81,7 @@ plt.savefig('count_type.png')
 """## Random Forest Classifier"""
 
 start_time = time.time()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42) # 80% for training, 20 for of testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 42) # 80% for training, 20 for of testing
 rf_clf = RandomForestClassifier(n_estimators=100, random_state=42, bootstrap=True)
 rf_clf.fit(X_train, y_train)
 y_pred = rf_clf.predict(X_test)
@@ -108,7 +108,7 @@ plt.xlabel('Predictions', fontsize=18)
 plt.ylabel('Actuals', fontsize=18)
 plt.title('Confusion Matrix', fontsize=18)
 plt.show()
-plt.savefig('Confusion_Matrix.png')
+plt.savefig('confusion_matrix.png')
 
 
 ### RF Accuracy Score
